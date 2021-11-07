1da6da975a8405f86c0a555e543d592946d52bf6
user <f01ga@mail.ru>
2021-11-07 19:38:50 +0300

    Script file

---
+++ script_dz_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd folder3
+mkdir test_s_1 test_s_2 test_s_3
+cd test_s_1
+touch qa_1.txt qa_2.txt qa_3.txt qa_4.json ga_5.json
+mkdir s_test_1 s_test_2 s_test_3
+ls -la
+mv qa_1.txt qa_4.json s_test_1
